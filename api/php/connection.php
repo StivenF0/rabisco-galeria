b400edc5216091ec5accdba4aac8f40a80d2b4b3
StivenF0 <stivenfonseca@gmail.com>
2022-12-19 22:18:09 -0300

    Little preventive changes

--- api/php/connection.php
+++ api/php/connection.php
@@ -2,7 +2,7 @@
 
 require_once('packets/vendor/autoload.php');
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../..');
 $dotenv->load();
 
 try {
